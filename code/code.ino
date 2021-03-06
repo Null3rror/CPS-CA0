377779b3ddd70ee64caae92457bfac18c3271ba9
Null3rror <null3rr0r@protonmail.com>
2021-03-06 23:00:59 +0330

    Feat: Add changing motor's direction
    Implemented changing direction from clockwise to anti-clockwise and vice versa.
    Implemented printing direction status and pause status.

--- code/code.ino
+++ code/code.ino
@@ -1,7 +1,4 @@
-// include Arduino stepper motor library
 #include <Stepper.h>
- 
-// change this to the number of steps on your motor
 #define STEPS 36
  
 
@@ -10,9 +7,6 @@ const int in2 = 9;
 const int in3 = 10;
 const int in4 = 11;
 
-// create an instance of the stepper class, specifying
-// the number of steps of the motor and the pins it's
-// attached to
 Stepper stepper(STEPS, in1, in2, in3, in4);
  
 const int stopButton         =  6; // direction control button is connected to Arduino pin 6
@@ -20,15 +14,19 @@ const int clockwiseButton    =  5; // direction control button is connected to A
 const int antiClockwisButton =  4; // direction control button is connected to Arduino pin 4
 
 boolean stopStatus;
+boolean stopPrevStatus;
 
 
  
 int direction_ = 1, rpm;
+int prevDirection = 1;
  
 
 void setup()
 {
   stopStatus = false;
+  stopPrevStatus = stopStatus;
+  prevDirection = direction_;
   rpm = 10;
   pinMode(in1, OUTPUT);
   pinMode(in2, OUTPUT);
@@ -43,8 +41,7 @@ void setup()
 }
 
 void loop()
-{
-  
+{ 
   if (!digitalRead(stopButton)) { // button down
     while(!digitalRead(stopButton)); // loop until button down, exit when button is up
     stopStatus = true;
@@ -52,64 +49,47 @@ void loop()
   if (!digitalRead(clockwiseButton)) {
     while(!digitalRead(clockwiseButton));
     stopStatus = false;
+    direction_ = 1;
   }
   if (!digitalRead(antiClockwisButton)) {
     while(!digitalRead(antiClockwisButton));
     stopStatus = false;
-  }
-  
-
-  if (stopStatus) {
-    Serial.println("Stopped");
- 
-  }
-  else {
-    Serial.println("Started");
+    direction_ = -1;
   }
 
-  
-  
+  if (HasStopStatusChanged()) {
+    PrintStopStatus();
+  }
+
+  if (HasDirChanged()) {
+    PrintDir();
+  }
   
   stepper.setSpeed(rpm);
-  
-  
-  // if button is pressed
-//    if ( debounce() )  // debounce button signal
-//    {
-//      direction_ *= -1;  // reverse direction variable
-//      while ( debounce() ) ;  // wait for button release
-//    }
- 
-  
-  int val = 10;
- 
-  // map digital value from [0, 1023] to [5, 100]
-  // ===> min speed = 5 and max speed = 100 rpm
-//  if ( speed_ != map(val, 0, 1023, 5, 100) )
-//  { // if the speed was changed
-//    speed_ = map(val, 0, 1023, 5, 100);
-//    // set the speed of the motor
-//    stepper.setSpeed(speed_);
-//  }
- 
-  // move the stepper motor
-  stepper.step(direction_);
- 
+
+  if (!stopStatus) {
+    // move the stepper motor
+    stepper.step(direction_);
+  }
+  stopPrevStatus = stopStatus;
+  prevDirection = direction_;
+
 }
 
-void stopMoving() { // TODO: better name
-  
+boolean HasStopStatusChanged() {
+  return stopStatus != stopPrevStatus;
 }
- 
-//// a small function for button debounce
-//bool debounce()
-//{
-//  byte count = 0;
-//  for(byte i = 0; i < 5; i++) {
-//    if (digitalRead(button) == 0)
-//      count++;
-//    delay(10);
-//  }
-//  if(count > 2)  return 1;
-//  else           return 0;
-//}
+
+boolean HasDirChanged() {
+  return direction_ != prevDirection;
+}
+
+
+void PrintStopStatus() {
+  Serial.println(stopStatus ? "Paused" : "UnPaused"); 
+}
+
+void PrintDir() {
+  Serial.print("direction: ");
+  Serial.println(direction_ > 0 ? "clockwise" : "antiClockwise");
+}

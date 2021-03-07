f97997467d1f4b42739da177160775f662c4416c
gnamiro <56019493+gnamiro@users.noreply.github.com>
2021-03-07 09:55:14 +0330

    Style(DC): Refactor DC.ino

--- DC/DC_motor.ino
+++ DC/DC_motor.ino
@@ -1,79 +1,86 @@
 // Arduino DC motor speed and direction control
  
-#define button   5
-#define pot      0
-#define pwm1     9
-#define pwm2    10
-#define increase 4
-#define decrease 3
-#define stop_start 2
+#define DIR_BUTTON          5  //CHANGE DIRECTION BUTTON
+#define POT                 0 
+#define PWM1                9
+#define PWM2                10
+#define INCREASE_BUTTON     4  //INCREASE MOTOR SPEED BUTTON
+#define DECREASE_BUTTON     3  //DECREASE MOTOR SPEED BUTTON
+#define STOP_START_BUTTON   2  //STOP and START BUTTON
  
-boolean motor_dir = 0;
-boolean stopb=0;
-boolean flag =0;
-int motor_speed;
-
+boolean motorDir         =  0;
+boolean stopStatus       =  0;
+boolean changeStatus     =  0;
+int motorSpeed;
  
 void setup() {
   Serial.begin(9600);
-  pinMode(button, INPUT_PULLUP);
-  pinMode(increase , INPUT_PULLUP);
-  pinMode(stop_start , INPUT_PULLUP);
-  pinMode(decrease, INPUT_PULLUP);  
-  pinMode(pwm1,   OUTPUT);
+ 
+  pinMode(DIR_BUTTON, INPUT_PULLUP);
+  pinMode(INCREASE_BUTTON , INPUT_PULLUP);
+  pinMode(STOP_START_BUTTON , INPUT_PULLUP);
+  pinMode(DECREASE_BUTTON, INPUT_PULLUP);  
+ 
+  pinMode(PWM1,   OUTPUT);
   pinMode(pwm2,   OUTPUT);
 
-motor_speed = analogRead(pot) / 4;
-Serial.println("start pwm : " +  String(motor_speed));
+  motorSpeed = analogRead(POT) / 4;
+  Serial.println("start pwm : " +  String(motorSpeed));
 }
  
 void loop() {
 
-   if(!digitalRead(stop_start)){                // If direction button is pressed
-    while(!digitalRead(stop_start));           // Wait until direction button released
-    stopb = !stopb;                // Toggle direction variable
+  if(!digitalRead(STOP_START_BUTTON)){
+   while(!digitalRead(STOP_START_BUTTON)); 
+   
+   stopStatus = !stopStatus;                
   }
-    if(stopb){
-    digitalWrite(pwm1, 0);
-    digitalWrite(pwm2, 0);
+ 
+  if(stopStatus){
+   digitalWrite(PWM1, 0);
+   digitalWrite(PWM2, 0);
+  }else{
+    if(!digitalRead(INCREASE_BUTTON)){              
+      while(!digitalRead(INCREASE_BUTTON));
+     
+      motorSpeed = motorSpeed  + 10;
+      changeStatus = 1;
     }
-    else{
-
-  if(!digitalRead(increase)){              
-    while(!digitalRead(increase));     
-  motor_speed = motor_speed  + 10;
-  flag =1;
-}
-  if(!digitalRead(decrease)){              
-    while(!digitalRead(decrease));     
-  motor_speed = motor_speed  - 10;
-  flag =1;
-}
-  if(motor_speed > 255){
-    motor_speed = 255;
-    flag =1;
-  }else if(motor_speed < 0){
-    motor_speed = 0;
-    flag =1;
-
-}
-if(flag){
-  Serial.println("current pwm : " + String(motor_speed));
-  flag =0;
-}  
-if(motor_dir)
-    analogWrite(pwm1, motor_speed);
-  else
-    analogWrite(pwm2, motor_speed);
-  
-
-  if(!digitalRead(button)){                // If direction button is pressed
-    while(!digitalRead(button));           // Wait until direction button released
-    motor_dir = !motor_dir;                // Toggle direction variable
-    if(motor_dir)
-      digitalWrite(pwm2, 0);
+   
+    if(!digitalRead(DECREASE_BUTTON)){              
+      while(!digitalRead(DECREASE_BUTTON));     
+      
+      motorSpeed = motorSpeed  - 10;
+      changeStatus = 1;
+    }
+   
+    if(motorSpeed > 255){
+      motorSpeed = 255;
+      changeStatus = 1;
+    }else if(motorSpeed < 0){
+      motorSpeed = 0;
+      changeStatus = 1;
+    }
+   
+    if(flag){
+      Serial.println("current pwm : " + String(motorSpeed));
+      changeStatus = 0;
+    }  
+   
+    if(motorDir)
+      analogWrite(PWM1, motorSpeed);
     else
-      digitalWrite(pwm1, 0);
-  }
+      analogWrite(PWM2, motorSpeed);
+
+
+    if(!digitalRead(DIR_BUTTON)){              
+     while(!digitalRead(DIR_BUTTON));  
+     
+     motorDir = !motorDir;               
+     if(motorDir)
+       digitalWrite(PWM2, 0);
+     else
+       digitalWrite(PWM1, 0);
     }
+  }
 }

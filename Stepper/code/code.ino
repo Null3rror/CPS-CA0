e0fc84f382db16a97f7185f05a53c8c05f075a06
farough1995 <farough.shayeste@gmail.com>
2021-03-09 02:40:29 +0330

    Final Changes
    3/9/2021 2:40 AM

--- Stepper/code/code.ino
+++ Stepper/code/code.ino
@@ -1,5 +1,5 @@
 #include <Stepper.h>
-#define STEPS 36
+#define STEPS 36 // total number of steps this motor can take
  
 
 const int IN1 = 8;
@@ -9,14 +9,14 @@ const int IN4 = 11;
 
 Stepper stepper(STEPS, IN1, IN2, IN3, IN4);
 
-const int STOP_BUTTON           =  6; // direction control button is connected to Arduino pin 6
-const int CLOCKWISE_BUTTON      =  5; // direction control button is connected to Arduino pin 5
-const int ANTI_CLOCKWISE_BUTTON =  4; // direction control button is connected to Arduino pin 4
+const int STOP_BUTTON           =  6; // motorDirection control button is connected to Arduino pin 6
+const int CLOCKWISE_BUTTON      =  5; // motorDirection control button is connected to Arduino pin 5
+const int ANTI_CLOCKWISE_BUTTON =  4; // motorDirection control button is connected to Arduino pin 4
 
 boolean stopStatus;
 boolean stopPrevStatus;
 
-int direction = 1, rpm;
+int motorDirection = 1, rpm;
 int prevDirection = 1;
  
 
@@ -24,8 +24,8 @@ void setup()
 {
   stopStatus = false;
   stopPrevStatus = stopStatus;
-  prevDirection = direction;
-  rpm = 10;
+  prevDirection = motorDirection;
+  rpm = 6;
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(IN3, OUTPUT);
@@ -47,12 +47,12 @@ void loop()
   if (!digitalRead(CLOCKWISE_BUTTON)) {
     while(!digitalRead(CLOCKWISE_BUTTON));
     stopStatus = false;
-    direction = 1;
+    motorDirection = 1;
   }
   if (!digitalRead(ANTI_CLOCKWISE_BUTTON)) {
     while(!digitalRead(ANTI_CLOCKWISE_BUTTON));
     stopStatus = false;
-    direction = -1;
+    motorDirection = -1;
   }
 
   if (HasStopStatusChanged()) {
@@ -67,10 +67,10 @@ void loop()
 
   if (!stopStatus) {
     // move the stepper motor
-    stepper.step(direction);
+    stepper.step(motorDirection);
   }
   stopPrevStatus = stopStatus;
-  prevDirection = direction;
+  prevDirection = motorDirection;
 
 }
 
@@ -79,7 +79,7 @@ boolean HasStopStatusChanged() {
 }
 
 boolean HasDirChanged() {
-  return direction_ != prevDirection;
+  return motorDirection != prevDirection;
 }
 
 
@@ -88,6 +88,6 @@ void PrintStopStatus() {
 }
 
 void PrintDir() {
-  Serial.print("direction: ");
-  Serial.println(direction > 0 ? "Clockwise" : "AntiClockwise");
+  Serial.print("motorDirection: ");
+  Serial.println(motorDirection > 0 ? "Clockwise" : "AntiClockwise");
 }
